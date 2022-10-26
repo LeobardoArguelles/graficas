f6b4a2bc9a791520e222273148c98b08a904da66
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2022-10-26 16:26:22 -0700

    Intento inicial

---
+++ build/CMakeFiles/ejercicio05.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio05.dir/common/shader.cpp.o"
+  "CMakeFiles/ejercicio05.dir/common/shader.cpp.o.d"
+  "CMakeFiles/ejercicio05.dir/e05/ejercicio05.cpp.o"
+  "CMakeFiles/ejercicio05.dir/e05/ejercicio05.cpp.o.d"
+  "ejercicio05"
+  "ejercicio05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

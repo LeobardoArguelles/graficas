630e552c59783a717a1f3eed3b6ef25afbf5edab
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2022-10-26 22:44:01 -0700

    Create cube

---
+++ build/CMakeFiles/ejercicio06.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio06.dir/common/shader.cpp.o"
+  "CMakeFiles/ejercicio06.dir/common/shader.cpp.o.d"
+  "CMakeFiles/ejercicio06.dir/e06/ejercicio06.cpp.o"
+  "CMakeFiles/ejercicio06.dir/e06/ejercicio06.cpp.o.d"
+  "ejercicio06"
+  "ejercicio06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

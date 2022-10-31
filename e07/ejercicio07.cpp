42f582dac0f2139058fd45e5a8b346feaf843932
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2022-10-30 21:47:32 -0700

    Delete previous code for text

--- e07/ejercicio07.cpp
+++ e07/ejercicio07.cpp
@@ -33,19 +33,7 @@ struct glm_ptr {
 	glm::vec3 theta;
 };
 
-struct Character {
-	unsigned int TextureID;
-	glm::ivec2 Size;
-	glm::ivec2 Bearing;
-	unsigned int Advance;
-};
-
-
-
-void RenderText(Shader &s, std::string text, float x, float y, float scale,
-glm::vec3 color, unsigned int VAO, unsigned int VBO, std::map<char, Character> Characters);
 void rotate(glm_ptr* ptr, glm::mat3 matrix);
-void mapChars(FT_Face face, std::map<char, Character> Characters);
 void rotateX(glm_ptr* ptr);
 void rotateY(glm_ptr* ptr);
 void rotateZ(glm_ptr* ptr);
@@ -86,27 +74,12 @@ int main(int argc, char **argv) {
 
 	// END BOILERPLATE
 
-	// USAR FREETYPE PARA TEXTO
-	FT_Library ft;
-	if (FT_Init_FreeType(&ft))
-		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-
-	FT_Face face;
-	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
-		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-
-	std::map<char, Character> Characters;
-	mapChars(face, Characters);
-
-	// (FT_Face, width, height): width en 0 permite que es calcule automaticamente
-	FT_Set_Pixel_Sizes(face, 0, 48);
 	// CALLBACKS
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 	// SHADERS
 	Shader shader("./shader.vs", "./shader.fs");
 	Shader shaderNT("./shader_no_transform.vs", "./shader.fs");
-	Shader shaderText("./shader_text.vs", "./text_shader.fs");
 
 	// 8 vértices, 6 datos por cada uno (3 posición, 3 color)
 	int nData = 8*6;
@@ -330,13 +303,13 @@ int main(int argc, char **argv) {
 
 		gltColor(1.0f, 1.0f, 1.0f, 1.0f);
 		//                              x     y     scale
-        gltDrawText2DAligned(text1, 115.0f, 70.0f, 1.0f,
+        gltDrawText2DAligned(text1, 115.0f, 60.0f, 1.0f,
                             GLT_CENTER, GLT_CENTER);
 
-        gltDrawText2DAligned(text2, 300.0f, 70.0f, 1.0f,
+        gltDrawText2DAligned(text2, 300.0f, 60.0f, 1.0f,
                             GLT_CENTER, GLT_CENTER);
 
-        gltDrawText2DAligned(text3, 480.0f, 70.0f, 1.0f,
+        gltDrawText2DAligned(text3, 480.0f, 60.0f, 1.0f,
                             GLT_CENTER, GLT_CENTER);
         gltEndDraw();
 
@@ -438,103 +411,8 @@ void rotateZ(glm_ptr* ptr) {
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
 	double xpos, ypos;
-	// if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
-	// 	glfwGetCursorPos(window, &xpos, &ypos);
-	// 	data* d = (data*)glfwGetWindowUserPointer(window);
-	// 	if (xpos < 600 && xpos > 500 && ypos < 100 && ypos > 0) {
-	// 		if (d->divisions < MAX_DIVISIONS) {
-	// 			increaseTriangle(d);
-	// 			moveSquare(d, true);
-	// 		}
-	// 	}
-	// 	else if (xpos > 0 && xpos < 100 && ypos < 100 && ypos > 0) {
-	// 		if (d->divisions > MIN_DIVISIONS) {
-	// 			decreaseTriangle(d);
-	// 			moveSquare(d);
-	// 		}
-	// 	}
-	// }
-}
-
-void mapChars(FT_Face face, std::map<char, Character> Characters) {
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // no byte-alignment restriction
-	for (unsigned char c = 0; c < 128; c++)
-	{
-		// load character glyph
-		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-		{
-			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-			continue;
-		}
-		// generate texture
-		unsigned int texture;
-		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glTexImage2D(
-			GL_TEXTURE_2D,
-			0,
-			GL_RED,
-			face->glyph->bitmap.width,
-			face->glyph->bitmap.rows,
-			0,
-			GL_RED,
-			GL_UNSIGNED_BYTE,
-			face->glyph->bitmap.buffer
-		);
-		// set texture options
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		// now store character for later use
-		Character character = {
-		texture,
-		glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-		glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-		face->glyph->advance.x
-		};
-		Characters.insert(std::pair<char, Character>(c, character));
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+		glfwGetCursorPos(window, &xpos, &ypos);
+		std::cout << xpos << " " << ypos << std::endl;
 	}
-}
-
-void RenderText(Shader &s, std::string text, float x, float y, float scale,
-glm::vec3 color, unsigned int VAO, unsigned int VBO, std::map<char, Character> Characters)
-{
-	// activate corresponding render state
-	s.use();
-	glUniform3f(glGetUniformLocation(s.ID, "textColor"),
-	color.x, color.y, color.z);
-	glActiveTexture(GL_TEXTURE0);
-	glBindVertexArray(VAO);
-	// iterate through all characters
-	std::string::const_iterator c;
-	for (c = text.begin(); c != text.end(); c++)
-	{
-		Character ch = Characters[*c];
-		float xpos = x + ch.Bearing.x * scale;
-		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-		float w = ch.Size.x * scale;
-		float h = ch.Size.y * scale;
-		// update VBO for each character
-		float vertices[6][4] = {
-			{ xpos, ypos + h, 0.0f, 0.0f },
-			{ xpos, ypos, 0.0f, 1.0f },
-			{ xpos + w, ypos, 1.0f, 1.0f },
-			{ xpos, ypos + h, 0.0f, 0.0f },
-			{ xpos + w, ypos, 1.0f, 1.0f },
-			{ xpos + w, ypos + h, 1.0f, 0.0f }
-		};
-		// render glyph texture over quad
-		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-		// update content of VBO memory
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		// render quad
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		// advance cursors for next glyph (advance is 1/64 pixels)
-		x += (ch.Advance >> 6) * scale; // bitshift by 6 (2^6 = 64)
-	}
-	glBindVertexArray(0);
-	glBindTexture(GL_TEXTURE_2D, 0);
 }
